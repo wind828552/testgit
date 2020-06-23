dbaba7fc126d9edb2af3caab8299e69b18cb5acb
wenkg <wind828552@gmail.com>
2020-06-23 12:32:36 +0800

    v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testgit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
